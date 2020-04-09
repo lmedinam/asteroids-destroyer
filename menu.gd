6f1d96f258c77b5b41a0f8a4687f1967bf2e0637
Luis Medina <luismedina.git@outlook.com>
2020-04-09 17:42:17 -0400

    F to pay respects

--- menu.gd
+++ menu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+func _ready():
+	if rand_range(0, 100) < 5:
+		$Header/ContentContainer/FullscreenWrapper/Label.text = "F to pay respects"
+
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		GameManager.change_scene("level")
